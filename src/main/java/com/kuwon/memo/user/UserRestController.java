a33b114ee7dd99c09447b21516a10d03b8d52f33
JW <kiw970923@naver.com>
2024-03-21 18:23:25 +0900

    [feat] 로그인 시 세션 정보 저장
     - 로그인 시 사용자 primary key, 이름을 session에 저장
    #2

--- src/main/java/com/kuwon/memo/user/UserRestController.java
+++ src/main/java/com/kuwon/memo/user/UserRestController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kuwon.memo.user.domain.User;
 import com.kuwon.memo.user.service.UserService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/user")
 @RestController // @Controller + @ResponseBody
 public class UserRestController {
@@ -39,13 +42,20 @@ public class UserRestController {
 	@PostMapping("/login")
 	public Map<String, String> login(
 			@RequestParam("loginId")String loginId
-			, @RequestParam("password")String password){
+			, @RequestParam("password")String password
+			, HttpServletRequest request){
 		User user = userService.getUserByLoginIdAndPassword(loginId, password);
 		
 		Map<String, String> resultMap = new HashMap<>();
 		
 		if(user != null) { // 로그인 성공
 			resultMap.put("result", "success");
+			// 조회된 사용자 정보를 세션에 저장
+			HttpSession session = request.getSession();
+			// 사용자 정보를 대표하는 값 : user의 primary key
+			session.setAttribute("userId", user.getId());
+			// 모든 페이지에서 빈번하게 사용되는 사용자 정보는 저장해주면 잦은 쿼리를 줄일 수 있음
+			session.setAttribute("userName", user.getName());
 		}else { // 로그인 실패
 			resultMap.put("result", "failure");
 		}
